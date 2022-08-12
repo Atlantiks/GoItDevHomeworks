7c1740b4c700c4af334fa623b4eb782c27480028
Sergii Shynkarenko <atlantiks@outlook.com>
2022-08-12 22:31:49 +0300

    sample added for unit tests

--- src/test/java/ua/goit/service/UserCartTest.java
+++
@@ -1,5 +0,0 @@
-package ua.goit.service;
-
-public class UserCartTest {
-
-}
--- src/test/java/ua/goit/service/UserServiceTest.java
+++ src/test/java/ua/goit/service/UserServiceTest.java
@@ -1,20 +1,5 @@
 package ua.goit.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import ua.goit.grocery.InMemoryDatabase;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class UserServiceTest {
 
-    @BeforeAll
-    static void init() {
-    }
-
-    @Test
-    void testFilterUserInput() {
-    }
 }
